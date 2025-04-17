1d2714708962793c88fc477aa3e1ec517db9ee25
loolkkekr <98670575+loolkkekr@users.noreply.github.com>
2025-04-17 10:33:47 +0300

    Output лимит изменён на 128к
    По умолчанию стоит 1к.

--- server.py
+++ server.py
@@ -20,11 +20,12 @@ def get_models():
         return jsonify([]), 500
 def call_deepseek_api(messages, provider="openrouter", model="openrouter:anthropic/claude-3.7-sonnet:thinking"):
 #def call_deepseek_api(messages, model="deepseek-reasoner"):
+    print(PUTER_API_TOKEN)
     url = "https://api.puter.com/drivers/call"
     headers = {
         "Authorization": "Bearer " + str(PUTER_API_TOKEN),
         "Content-Type": "application/json",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
         "Origin": "https://docs.puter.com"
     }
 
@@ -34,6 +35,7 @@ def call_deepseek_api(messages, provider="openrouter", model="openrouter:anthrop
         "interface": "puter-chat-completion",
         "driver": str(provider),
         "test_mode": False,
+        "max_tokens": 131072,
         "method": "complete",
         "args": {
             "messages": messages,  # Теперь принимаем полную историю сообщений
