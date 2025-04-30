5922a5b0f485a864108a4bd87d4c0e4026a456b4
loolkkekr <83313652+loolkkekrr@users.noreply.github.com>
2025-04-30 23:05:31 +0300

    Обновление серверной части.
    = Изменена по умолчанию модель на GPT-4o Mini.
    - Удалена Debug информация.

--- server.py
+++ server.py
@@ -18,8 +18,7 @@ def get_models():
     except Exception as e:
         print(f"Error loading models: {e}")
         return jsonify([]), 500
-def call_deepseek_api(messages, provider="openrouter", model="openrouter:anthropic/claude-3.7-sonnet:thinking", max_tokens=2048, temperature=1):
-#def call_deepseek_api(messages, model="deepseek-reasoner"):
+def call_deepseek_api(messages, provider="openai-completion", model="gpt-4o-mini", max_tokens=2048, temperature=1):
     url = "https://api.puter.com/drivers/call"
     headers = {
         "Authorization": "Bearer " + str(PUTER_API_TOKEN),
@@ -27,9 +26,6 @@ def call_deepseek_api(messages, provider="openrouter", model="openrouter:anthrop
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0",
         "Origin": "https://docs.puter.com"
     }
-    print("МАКС. ТОКЕНОВ: " + str(max_tokens))
-    print("МОДЕЛЬ: " + model)
-    print("ПРОВАЙДЕР: " + provider)
     payload = {
         "interface": "puter-chat-completion",
         "driver": str(provider),
